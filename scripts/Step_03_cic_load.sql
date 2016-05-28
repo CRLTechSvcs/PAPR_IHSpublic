5b09a4244feda73cf5844cbec2e4f045878d20e7
Reza Rahim <rahimre@yahoo.com>
2016-05-27 22:29:48 -0500

    fix the for custom date

--- scripts/Step_03_cic_load.sql
+++ scripts/Step_03_cic_load.sql
@@ -1,13 +1,13 @@
-DROP DATABASE IF EXISTS `data`;
+-- DROP DATABASE IF EXISTS `data`;
 
-CREATE DATABASE `data`;
+-- CREATE DATABASE `data`;
 
 use `data`;
--- drop table data.tmp_article;
--- drop table data.tmp_issue;
--- drop table data.tmp_issue_idx ;
+drop table tmp_article;
+drop table tmp_issue;
+drop table tmp_issue_idx ;
 
-create table data.tmp_article(
+create table tmp_article(
 OCLC_Number	varchar(1000),
 Print_ISSN	varchar(1000),
 Electronic_ISSN	varchar(1000),
@@ -17,7 +17,8 @@ Pub_Start_Year varchar(1000),
 Pub_End_Year varchar(1000),	
 Language	varchar(1000),
 Country	varchar(1000),
-LC_Class varchar(1000)
+LC_Class varchar(1000),
+volumeLevelFlag varchar(1000)
 );
 
 create table tmp_issue (
@@ -37,21 +38,34 @@ issue_type	varchar(1000),
 issue_title varchar(1000)
 );
 
-create table data.tmp_issue_idx (
+create table tmp_issue_idx (
 id int(11) NOT NULL AUTO_INCREMENT,
 oclc_number	varchar(1000),
 year	varchar(1000),
 month	varchar(1000),
-first_level_number	varchar(1000),
-second_level_number	varchar(1000),
+volumeNumber	varchar(1000),
+issueNumber	varchar(1000),
 PRIMARY KEY (`id`)
 );
 
 
-LOAD DATA LOCAL INFILE 'C:\\travant\\data\\ihs\\CIC_data_for_IHS_artcle_utf8.txt' into table tmp_article   FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\r\n';
+-- LOAD DATA LOCAL INFILE 'C:\\travant\\data\\release\\data\\bib-level-data.csv' into table tmp_article   FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n';
 
-LOAD DATA LOCAL INFILE 'C:\\travant\\data\\ihs\\CIC_data_for_IHS_issue.txt' into table tmp_issue  CHARACTER SET 'latin1' FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\r\n';
+-- LOAD DATA LOCAL INFILE 'C:\\travant\\data\\release\\data\\issue-level-data.csv' into table tmp_issue  CHARACTER SET 'latin1' FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n';
 
-insert into  data.tmp_issue_idx( oclc_number, year, month, first_level_number, second_level_number ) 
+
+LOAD DATA LOCAL INFILE 'C:\\travant\\data\\release\\data\\volume-only-bib-data.csv' into table tmp_article   FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n';
+
+LOAD DATA LOCAL INFILE 'C:\\travant\\data\\release\\data\\volume-only-issue-data.csv' into table tmp_issue  CHARACTER SET 'latin1' FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n';
+
+
+insert into  tmp_issue_idx( oclc_number, year, month, volumeNumber, issueNumber ) 
 select oclc_number, year, month, first_level_number, second_level_number 
-from data.tmp_issue;
+from tmp_issue;
+
+
+-- check the value of the tmp tables
+
+-- select * from tmp_article;
+-- select * from data.tmp_issue;
+-- select * from tmp_issue_idx;
