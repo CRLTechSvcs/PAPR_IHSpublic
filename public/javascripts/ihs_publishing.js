635fa0d612dfbfbe1ceb1da8f3fa8d0970ec6a3e
Andy Elliott <aelliott@crl.edu>
2016-11-02 11:25:52 -0500

    console messages to investigate why the month for startDate and endDate are always January

--- public/javascripts/ihs_publishing.js
+++ public/javascripts/ihs_publishing.js
@@ -7,14 +7,14 @@ require(["dijit/TitlePane","dojo/dom","dojo/domReady!"], function(dom) {
         url: "/publishing/getPublishingView",
         preventCache: true,
         load: function(data) {
-        	
+
         	publishingView = data;
         },
         error: function(e) {
             alert("Error: " + e.message);
         }
     });
-	
+
 	$(function() {
 		 $('#startDate').datepicker();
 		 $('#endDate').datepicker();
@@ -22,46 +22,47 @@ require(["dijit/TitlePane","dojo/dom","dojo/domReady!"], function(dom) {
 });
 
 function submitPub(){
-	
+
 	var jobName = $('#jobName').val();
 	var fileFormat = $('#fileFormat').val();
 	var startDate =  $('#startDate').val();
 	var endDate =  $('#endDate').val();
-	
+
 	var errorMessage = '';
-	
+
 	if(jobName == ''){
 		errorMessage += 'Enter a  valid Job Name \n' ;
 	}
-	
-	
+
+
 	if(startDate != ''){
 		if(  !validateDate(startDate) ){
 			errorMessage += 'Enter a  valid Start Date \n' ;
-		
-		} 
+
+		}
 	}
-	
+
 	if( endDate != ''){
 		if( !validateDate(endDate) ){
 			errorMessage += 'Enter a  valid End Date \n' ;
-			
+
 		}
 	}
-	
+
 	if(errorMessage != ''){
 		alert(errorMessage);
 		return;
 	}
-	 
-	
+
+
 	publishingView.jobName = jobName;
 	publishingView.fileFormat = fileFormat;
 	publishingView.startDate = startDate;
 	publishingView.endDate = endDate;
-	
+console.warn('startDate = ', startDate, '; endDate = ', endDate);
+
 	showWaiting();
-	
+
 	dojo.rawXhrPost({
         url: "/publishing/postPublishingView",
         postData: dojo.toJson(publishingView),
@@ -70,16 +71,16 @@ function submitPub(){
             "Accept": "application/json"
         },
         handleAs: "text",
-        
+
         load: function(data) {
         	hideWaiting();
    	 		window.location="/publishing_published_data";
-        	
+
         },
         error: function(error) {
         	 hideWaiting();
             alert("Error:" + error);
         }
     });
-	
+
 }
