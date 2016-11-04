8952631afb1e07c88fdc05e852367ab20d5472dd
Andy Elliott <aelliott@crl.edu>
2016-11-04 13:54:24 -0500

    whitespace and comments

--- app/actors/PublishingJobActor.java
+++ app/actors/PublishingJobActor.java
@@ -40,8 +40,7 @@ public class PublishingJobActor extends UntypedActor {
 	@Override
 	public void onReceive(Object message) throws Exception {
 
-    Logger.info("PublishingJobActor.onReceive begins with message=" +message); // AJE 2016-09-30
-    Logger.info("and (message instanceof Integer) == "+ (message instanceof Integer)+ ".");
+    Logger.info("PublishingJobActor.onReceive begins with message=" +message+ "; (message instanceof Integer) == "+ (message instanceof Integer)+ ".");
 
 		if (message instanceof Integer) {
 
@@ -55,30 +54,33 @@ public class PublishingJobActor extends UntypedActor {
 			String link = "";
 
 			Logger.info("PublishingJobActor.onReceive. 'Start Running Publishing job': "+
-			  "ihsPublishingJob.startDate='" +ihsPublishingJob.startDate+"', "+
-			  "ihsPublishingJob.endDate='"+ihsPublishingJob.endDate+"'.");
-
-      Logger.info("In the dates , the year and day of month is probably right, but note that the month is always '01'?");
+			  "ihsPublishingJob.startDate='" +ihsPublishingJob.startDate+"', ihsPublishingJob.endDate='"+ihsPublishingJob.endDate+"'.");
+      Logger.info("In the dates, year + day of month is probably right, but note month is always '01'?");
 
 			if (ihsPublishingJob.startDate != null
 					&& ihsPublishingJob.endDate != null) {
+        Logger.info("PublishingJobActor.onReceive: enter if startDate AND endDate NOT null block");
 				ihsTitles = IhsTitle.find.fetch("ihsPublisher")
 						.fetch("ihsVolume").fetch("ihsVolume.ihsissues")
 						.fetch("ihsPublicationRange").where()
 						.ge("changeDate", ihsPublishingJob.startDate)
 						.le("changeDate", ihsPublishingJob.endDate).findList();
 			} else if (ihsPublishingJob.startDate != null) {
+  			Logger.info("PublishingJobActor.onReceive: enter if startDate NOT null block");
+
 				ihsTitles = IhsTitle.find.fetch("ihsPublisher")
 						.fetch("ihsVolume").fetch("ihsVolume.ihsissues")
 						.fetch("ihsPublicationRange").where()
 						.ge("changeDate", ihsPublishingJob.startDate)
 						.findList();
 			} else if (ihsPublishingJob.endDate != null) {
+			  Logger.info("PublishingJobActor.onReceive: enter if endDate NOT null block");
 				ihsTitles = IhsTitle.find.fetch("ihsPublisher")
 						.fetch("ihsVolume").fetch("ihsVolume.ihsissues")
 						.fetch("ihsPublicationRange").where()
 						.le("changeDate", ihsPublishingJob.endDate).findList();
 			} else {
+			  Logger.info("PublishingJobActor.onReceive: enter ELSE block: one of startDate or endDate was null");
 				ihsTitles = IhsTitle.find.fetch("ihsPublisher")
 						.fetch("ihsVolume").fetch("ihsVolume.ihsissues")
 						.fetch("ihsPublicationRange").findList();
@@ -90,38 +92,18 @@ public class PublishingJobActor extends UntypedActor {
 
 			try {
 				if (ihsPublishingJob.fileformat == 1) {
-
-					Logger.info("ihsPublishingJob.fileformat == "+ihsPublishingJob.fileformat+
-					  ", thus will call buildMArk. The value of 'link' is never set in this block.");
-
-          // AJE 2016-09-30 buildMArk was delivered as empty brackets by Travant
-					buildMArk(ihsTitles);
-
+					Logger.info("ihsPublishingJob.fileformat == " +ihsPublishingJob.fileformat+ ", thus will call buildMArk. Value of 'link' is never set in this block.");
+					buildMArk(ihsTitles); // AJE 2016-09-30 buildMArk was delivered as empty brackets by Travant
 				} else if (ihsPublishingJob.fileformat == 2) {
-
-					Logger.info("ihsPublishingJob.fileformat == "+ihsPublishingJob.fileformat+
-  					", thus will call buildPortico, which may cause "+
-  					"'java.lang.OutOfMemoryError: Java heap space.'"+
-            " This is the only block where the value of 'link' is set.");
-
+					Logger.info("ihsPublishingJob.fileformat == " +ihsPublishingJob.fileformat+ ", thus will call buildPortico ; this Portico block is the only one where value of 'link' is set.");
           link = buildPortico(ihsTitles);
-
+					Logger.info("after buildPortico, link = " +link+ ".");
 				} else if (ihsPublishingJob.fileformat == 3) {
-
-					Logger.info("ihsPublishingJob.fileformat == "+ihsPublishingJob.fileformat+
-					  ", thus will call buildIhsXls. The value of 'link' is never set in this block.");
-
-          // AJE 2016-09-30 buildIhsXls was delivered as empty brackets by Travant
-					buildIhsXls(ihsTitles);
-
+					Logger.info("ihsPublishingJob.fileformat == " +ihsPublishingJob.fileformat+ ", thus will call buildIhsXls. Value of 'link' is never set in this block.");
+					buildIhsXls(ihsTitles); // AJE 2016-09-30 buildIhsXls was delivered as empty brackets by Travant
 				} else if (ihsPublishingJob.fileformat == 4) {
-
-					Logger.info("ihsPublishingJob.fileformat == "+ihsPublishingJob.fileformat+
-					  ", thus will call buildIhsCsv. The value of 'link' is never set in this block.");
-
-          // AJE 2016-09-30 buildIhsCsv was delivered as empty brackets by Travant
-					buildIhsCsv(ihsTitles);
-
+					Logger.info("ihsPublishingJob.fileformat == " +ihsPublishingJob.fileformat+ ", thus will call buildIhsCsv. Value of 'link' is never set in this block.");
+					buildIhsCsv(ihsTitles); // AJE 2016-09-30 buildIhsCsv was delivered as empty brackets by Travant
 				}
 
 				SingestionJobStatus singestionJobStatus = (SingestionJobStatus) SingestionJobStatus.find
@@ -150,22 +132,24 @@ public class PublishingJobActor extends UntypedActor {
 				ihsPublishingJob.update();
 				**********************************************************/
 
-      /*
-        AJE 2016-09-30
-      */
-
-				ihsPublishingJob.setLink("<a href=/public/exported/AJE_dummy_export.txt>" + "Download</a>");
 /*
 AJE 2016-09-30
 NOTE THAT UNLIKE THE SECTION 2 CALLING buildPortico,
 CALLING THE OTHER build* FUNCTIONS DOES NOT SET link = THE RETURN VALUE OF THOSE FUNCTIONS
 */
+/*
+Logger.info("before AJE resets link, link = " +link+ ".");
+String dataDir = Play.application().configuration().getString("application.publishing.process.data.Dir");
+//ihsPublishingJob.setLink("<a href='/"+dataDir+"/"+fileName+"'>Download</a>");
+Logger.info("after AJE resets link, link = " +link+ ".");
+*/
+
 
 Logger.info("PublishingJobActor.java, onReceive, BEFORE replace has ihsPublishingJob.link = " +ihsPublishingJob.link);
-Logger.info("... using AJE simplified link.replace next");
+Logger.info("... using AJE simplified link.replace next ; buildPortico DOES NOT ALWAYS RETURN A VALID LINK");
 link = link.replace('\\','/');
+ihsPublishingJob.setLink("<a href=" + link + ">Download</a>");
 Logger.info("PublishingJobActor.java, onReceive, AFTER replace has ihsPublishingJob.link = " +ihsPublishingJob.link);
-//ihsPublishingJob.setLink("<a href=" + link + ">Download</a>");
 ihsPublishingJob.update();
 //Logger.info("... NEW GET in routes for /public/reports ; new value in application.conf for application.PUBLISHING.process.data.Dir.  Yes, publishing is supposedly a different thing from reporting, and yes, there is an application.REPORTING.process.data.Dir");
 //Logger.info("AJE 2016-09-30 why no 'href' in link: " +link+ " ?  It is present in database.ihsreportingjob.link field.");
@@ -182,14 +166,13 @@ ihsPublishingJob.update();
 
 				ihsPublishingJob.setSingestionJobStatus(singestionJobStatus);
 				ihsPublishingJob.update();
-				Logger.error("PublishingJobActor has error: ", e);
+				Logger.error("PublishingJobActor.java: onReceive: has Exception: ", e);
 			}
 		} // end if (message instanceof Integer)
 
 	} // end onReceive
 
-	void buildMArk(List<IhsTitle> ihsTitles) {
-	  // AJE 2016-09-30 read the Logger: there was nothing here before today
+	void buildMArk(List<IhsTitle> ihsTitles) {// AJE 2016-09-30 read the Logger: there was nothing here before today
     Logger.info("buildMArk was delivered as empty brackets by Travant: all it does it print this message"); // AJE 2016-09-30
 	}
 
@@ -199,18 +182,15 @@ ihsPublishingJob.update();
     Logger.info("PublishingJobActor.java: enter buildPortico: did not crash when only a 5 day range chosen, but did for 30"); // AJ 2016-09-30
 
 
-		String dataDir = Play.application().configuration()
-				.getString("application.publishing.process.data.Dir");
-
+		String dataDir = Play.application().configuration().getString("application.publishing.process.data.Dir");
     Logger.info("buildPortico has dataDir = " +dataDir);
 
 		String fileName = rand.nextInt(10000) + "-" + "portico.xlsx";
-
     Logger.info("buildPortico has fileName = " +fileName);
 
-		String destFileString = dataDir + File.separator + fileName;
-
-    Logger.info("buildPortico has destFileString = " +destFileString);
+		// String destFileString = dataDir + File.separator + fileName; // Travant original
+		String destFileString = dataDir + System.getProperty("file.separator") + fileName; // AJE replaced 2016-11-02
+    Logger.info("buildPortico has NEW destFileString = " +destFileString);
 
 		Workbook workbook = null;
 
@@ -224,7 +204,6 @@ ihsPublishingJob.update();
 		Cell headerpubcell = headedrow.createCell(0);
 		headerpubcell.setCellValue("Publisher");
 		sheet.setColumnWidth(0, 10000);
-
 
 		Cell headertitlecell = headedrow.createCell(1);
 		headertitlecell.setCellValue("Title");
@@ -261,8 +240,9 @@ ihsPublishingJob.update();
 		CellStyle style = workbook.createCellStyle();
 		style.setWrapText(true);
 
-		for (IhsTitle ihsTitle : ihsTitles) {
+    Logger.info("buildPortico: before for line 254; ihsTitles.size()=" +ihsTitles.size());
 
+		for (IhsTitle ihsTitle : ihsTitles) {
 
       Logger.info("buildPortico: for (IhsTitle ihsTitle : ihsTitles) rowIndex)" +rowIndex);
 
@@ -297,6 +277,9 @@ ihsPublishingJob.update();
 			StringBuilder builderHolding = new StringBuilder();
 			boolean startVolume = true;
 			boolean startIssue = true;
+
+			Logger.info("buildPortico: line 292 before for(IhsVolume ihsVolume: ihsTitle.ihsVolume); ihsTitle.ihsVolume.size()=" +ihsTitle.ihsVolume.size());
+
 			for(IhsVolume ihsVolume: ihsTitle.ihsVolume){
 
       Logger.info("buildPortico: for(IhsVolume ihsVolume: ihsTitle.ihsVolume) rowIndex)" +rowIndex);
