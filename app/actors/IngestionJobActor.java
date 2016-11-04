da42e918d2d0244fd3ebf32d037196515a898a62
Andy Elliott <aelliott@crl.edu>
2016-11-04 13:54:17 -0500

    Exception every 30 seconds or so 'models.SingestionJobStatus cannot be cast to models.SingestionJobStatus' ; removing casting '(SingestionJobStatus)' does not resolve it

--- app/actors/IngestionJobActor.java
+++ app/actors/IngestionJobActor.java
@@ -25,7 +25,7 @@ public class IngestionJobActor extends UntypedActor {
 	boolean running = false;
 	IhsIngestionJob ihsIngestionJob = null;
 	String timeOut = "30 seconds";
-	
+
 
 	// LoggingAdapter log = Logging.getLogger(getContext().system(), this);
 
@@ -41,9 +41,12 @@ public class IngestionJobActor extends UntypedActor {
 		/*
 		 * Get oldest queued job from the database
 		 */
-		
-		SingestionJobStatus singestionJobStatus = (SingestionJobStatus) SingestionJobStatus.find.where().eq("name", SingestionJobStatus.Queued).findUnique();
-		
+
+		// next is Travant original line: casting (SingestionJobStatus) causes an error every 30 seconds or so "models.SingestionJobStatus cannot be cast to models.SingestionJobStatus"
+		//SingestionJobStatus singestionJobStatus = (SingestionJobStatus) SingestionJobStatus.find.where().eq("name", SingestionJobStatus.Queued).findUnique();
+		// AJE 2016-11-02 new test version : has no effect, still
+		SingestionJobStatus singestionJobStatus = SingestionJobStatus.find.where().eq("name", SingestionJobStatus.Queued).findUnique();
+
 		List<IhsIngestionJob> ihsIngestionJobs = IhsIngestionJob.find.where()
 				.eq("singestionJobStatus.ingestionJobStatusID", singestionJobStatus.ingestionJobStatusID)
 				.orderBy("ingestionJobID desc").setMaxRows(1).findList();
@@ -59,7 +62,7 @@ public class IngestionJobActor extends UntypedActor {
 
 	@Override
 	public void onReceive(Object message) throws Exception {
-		
+
 		try {
 			if (message instanceof ReceiveTimeout) {
 
@@ -70,82 +73,82 @@ public class IngestionJobActor extends UntypedActor {
 
 					Logger.debug("IngestionJobActor:Let's find a queued job");
 					ihsIngestionJob = null;
-					
+
 					ihsIngestionJob = checkForQueuedJob();
 
 					if (ihsIngestionJob != null) {
 						Logger.debug("IngestionJobActor: "
 								+ ihsIngestionJob.sourceFileString);
-						
-								
+
+
 						SingestionJobStatus singestionJobStatus = SingestionJobStatus.find.where().eq("name", SingestionJobStatus.Processing).findUnique();
-						
-						SqlUpdate update = Ebean.createSqlUpdate(IhsIngestionJob.upDateSql);  
+
+						SqlUpdate update = Ebean.createSqlUpdate(IhsIngestionJob.upDateSql);
 						update.setParameter("ingestionJobStatusID", singestionJobStatus.ingestionJobStatusID);
 						update.setParameter("ingestionJobID", ihsIngestionJob.ingestionjobID);
 						Ebean.execute(update);
-						
-								
+
+
 						fileReaderActor.tell(ihsIngestionJob, getSelf());
-						
+
 					} else {
 						Logger.debug("IngestionJobActor: No queued job");
 					}
 				}
 			} else if (message instanceof StatusMessage) {
 
-					
+
 					if (StatusMessage.SUCCESS
 							.equals(((StatusMessage) message).code)) {
 
 						Logger.debug("IngestionJobActor: Done running job: "
 								+ ihsIngestionJob.name);
-						
+
 						SingestionRecordStatus singestionRecordStatus = SingestionRecordStatus.find
 								.where().eq("name", SingestionRecordStatus.BadRecordError).findUnique();
-						
+
 						List<IhsIngestionRecord> ihsIngestionRecords = IhsIngestionRecord.find.where()
 								.eq("ihsIngestionJob", ihsIngestionJob)
 								.eq("singestionRecordStatus", singestionRecordStatus).findList();
-						
-						
 
-						if ( ihsIngestionRecords.size() > 0){ 
-						    //Get all the record with bad foramt and write to a static file 
+
+
+						if ( ihsIngestionRecords.size() > 0){
+						    //Get all the record with bad foramt and write to a static file
 							String sourceFileString = ihsIngestionJob.sourceFileString + IhsIngestionJob.badExten;
 							FileWriter outFile =  new FileWriter(sourceFileString);
 							PrintWriter out = new PrintWriter(outFile);
-							 
-							for(IhsIngestionRecord ihsIngestionRecord:  ihsIngestionRecords){ 
+
+							for(IhsIngestionRecord ihsIngestionRecord:  ihsIngestionRecords){
 								out.println( ihsIngestionRecord.rawRecordData);
 							}
-							out.close();  
-						
+							out.close();
+
 						}
-						
+
 						SingestionJobStatus singestionJobStatus = SingestionJobStatus.find.where().eq("name", SingestionJobStatus.Complete).findUnique();
-						
-						SqlUpdate update = Ebean.createSqlUpdate(IhsIngestionJob.upDateSql);  
+
+						SqlUpdate update = Ebean.createSqlUpdate(IhsIngestionJob.upDateSql);
 						update.setParameter("ingestionJobStatusID", singestionJobStatus.ingestionJobStatusID);
 						update.setParameter("ingestionJobID", ihsIngestionJob.ingestionjobID);
 						Ebean.execute(update);
-						
+
 					} else if (StatusMessage.ERROR
 							.equals(((StatusMessage) message).code)) {
 
 						Logger.debug("IngestionJobActor: Error running job: "
 								+ ihsIngestionJob.name);
-						
+
 						SingestionJobStatus singestionJobStatus = SingestionJobStatus.find.where().eq("name", SingestionJobStatus.FileProcessingError).findUnique();
-						
-						SqlUpdate update = Ebean.createSqlUpdate(IhsIngestionJob.upDateSql);  
+
+						SqlUpdate update = Ebean.createSqlUpdate(IhsIngestionJob.upDateSql);
 						update.setParameter("ingestionJobStatusID", singestionJobStatus.ingestionJobStatusID);
 						update.setParameter("ingestionJobID", ihsIngestionJob.ingestionjobID);
 						Ebean.execute(update);
-						
-					} 
+
+					}
 					running = false;
-					
+
 			} else {
 				unhandled(message);
 			}
@@ -153,16 +156,16 @@ public class IngestionJobActor extends UntypedActor {
 			running = false;
 			Logger.error("IhsIngestionJob Exception",e); // AJE 2016-08-25 this line was commented out
 			Logger.info("IhsIngestionJob Exception",e); // AJE 2016-08-25 this line is new
-			
+
 			SingestionJobStatus singestionJobStatus = SingestionJobStatus.find.where().eq("name", SingestionJobStatus.InternalError).findUnique();
-			
-			SqlUpdate update = Ebean.createSqlUpdate(IhsIngestionJob.upDateSql);  
+
+			SqlUpdate update = Ebean.createSqlUpdate(IhsIngestionJob.upDateSql);
 			update.setParameter("ingestionJobStatusID", singestionJobStatus.ingestionJobStatusID);
 			update.setParameter("ingestionJobID", ihsIngestionJob.ingestionjobID);
 			Ebean.execute(update);
-			
+
 		}finally{
-			
+
 		}
 		// getContext().stop(getSelf());
 	}
