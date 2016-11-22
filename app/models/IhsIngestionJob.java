51eacda4f8ae9b936650e7410fd9f748f5381303
Andy Elliott <aelliott@crl.edu>
2016-11-22 11:38:56 -0600

    whitespace

--- app/models/IhsIngestionJob.java
+++ app/models/IhsIngestionJob.java
@@ -28,11 +28,11 @@ import java.util.List;
 public class IhsIngestionJob extends Model {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
     private static final int maxrecord = 50;
-	 
+
     public static final String badExten = ".bad.txt";
 	public static String  upDateSql = "UPDATE ihsingestionjob SET ingestionJobStatusID = :ingestionJobStatusID WHERE ingestionJobID = :ingestionJobID";
 
@@ -40,50 +40,50 @@ public class IhsIngestionJob extends Model {
     @GeneratedValue
     @Column(name="ingestionJobID")
     public int ingestionjobID;
-    
+
     @Column(name="name")
     public String name;
-    
-    
+
+
     @Column(name="description")
     public String description;
-    
+
     @ManyToOne(fetch=FetchType.LAZY)
     @JoinColumn(name="authorizedSourceID")
     public IhsAuthorizedSource authorizedSource;
-    
+
     @ManyToOne(fetch=FetchType.LAZY)
     @JoinColumn(name="ingestionDataTypeID")
     public SingestionDatatype singestionDatatype;
-    
+
     @Column(name="creationDate")
     public Timestamp creationDate;
-    
+
     @ManyToOne(fetch=FetchType.LAZY)
     @JoinColumn(name="ingestedByUserID")
     public IhsUser ihsUser;
-    
+
     @Column(name="sourceFileString")
     public String sourceFileString;
-    
+
     @ManyToOne(fetch=FetchType.LAZY)
     @JoinColumn(name="ingestionJobStatusID")
     public SingestionJobStatus singestionJobStatus;
-    
+
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "ihsIngestionJob", cascade=CascadeType.ALL)
 	public List<IhsIngestionRecord> ihsIngestionRecords = new ArrayList<>();
-    
+
     @Column(name="statusDetail")
     public String statusDetail;
-    
+
     @ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "commitmentID")
 	public Scommitment scommitment;
-    
-    public IhsIngestionJob(String name, String description,IhsAuthorizedSource authorizedSource, SingestionDatatype singestionDatatype,  
+
+    public IhsIngestionJob(String name, String description,IhsAuthorizedSource authorizedSource, SingestionDatatype singestionDatatype,
     		Timestamp creationDate, IhsUser ihsUser,String sourceFileString, SingestionJobStatus singestionJobStatus,String statusDetail )
     {
-    	
+
     	this.name = name;
     	this.description = description;
     	this.authorizedSource = authorizedSource;
@@ -94,11 +94,11 @@ public class IhsIngestionJob extends Model {
 		this.singestionJobStatus = singestionJobStatus;
 		this.statusDetail = statusDetail;
     }
-    
-    public IhsIngestionJob(String name, String description,IhsAuthorizedSource authorizedSource, SingestionDatatype singestionDatatype,  
+
+    public IhsIngestionJob(String name, String description,IhsAuthorizedSource authorizedSource, SingestionDatatype singestionDatatype,
     		Timestamp creationDate, IhsUser ihsUser,String sourceFileString, SingestionJobStatus singestionJobStatus,String statusDetail, Scommitment scommitment  )
     {
-    	
+
     	this.name = name;
     	this.description = description;
     	this.authorizedSource = authorizedSource;
@@ -110,13 +110,12 @@ public class IhsIngestionJob extends Model {
 		this.statusDetail = statusDetail;
 		this.scommitment = scommitment;
     }
-    
-    public static Finder<Integer, IhsIngestionJob> find = new Finder<Integer, IhsIngestionJob>(
-			Integer.class, IhsIngestionJob
-			.class);
-       
 
-    
+    public static Finder<Integer, IhsIngestionJob> find = new Finder<Integer, IhsIngestionJob>(
+			Integer.class, IhsIngestionJob.class);
+
+
+
 	public static List<IhsIngestionJob> getAllUserJob() {
 
 		List<IhsIngestionJob> ihsIngestionJobs = IhsIngestionJob.find
@@ -124,10 +123,10 @@ public class IhsIngestionJob extends Model {
 				.fetch("ihsUser")
 				.fetch("singestionJobStatus")
 				.orderBy("ingestionJobID desc").setMaxRows(maxrecord).findList();
-		
+
 		return ihsIngestionJobs;
 	}
-	
+
 	public static List<IhsIngestionJob> getAUserJob(IhsUser ihsUser) {
 
 		List<IhsIngestionJob> ihsIngestionJobs = IhsIngestionJob.find
@@ -139,13 +138,13 @@ public class IhsIngestionJob extends Model {
 				.orderBy("ingestionJobID desc")
 				.setMaxRows(maxrecord)
 				.findList();
-		
+
 		return ihsIngestionJobs;
 	}
-	
+
 	public static List<IhsIngestionJob> getAMemberJob(IhsUser ihsUser) {
 
-		
+
 		List<IhsIngestionJob> ihsIngestionJobs = IhsIngestionJob.find
 				.fetch("authorizedSource")
 				.fetch("ihsUser")
@@ -155,14 +154,14 @@ public class IhsIngestionJob extends Model {
 				.orderBy("ingestionJobID desc")
 				.setMaxRows(maxrecord)
 				.findList();
-		
+
 		return ihsIngestionJobs;
 	}
-	
+
 	public static List<IhsIngestionJob> getAllQuedUserJob() {
 
 		SingestionJobStatus singestionJobStatus = SingestionJobStatus.find.where().eq("name", SingestionJobStatus.Queued ).findUnique();
-		
+
 		List<IhsIngestionJob> ihsIngestionJobs = IhsIngestionJob.find
 				.fetch("authorizedSource")
 				.fetch("ihsUser")
@@ -172,14 +171,14 @@ public class IhsIngestionJob extends Model {
 				.orderBy("ingestionJobID desc")
 				.setMaxRows(maxrecord)
 				.findList();
-		
+
 		return ihsIngestionJobs;
 	}
-	
+
 	public static List<IhsIngestionJob> getAllArchiveUserJob() {
 
 		SingestionJobStatus singestionJobStatus = SingestionJobStatus.find.where().eq("name", SingestionJobStatus.Processing ).findUnique();
-		
+
 		List<IhsIngestionJob> ihsIngestionJobs = IhsIngestionJob.find
 				.fetch("authorizedSource")
 				.fetch("ihsUser")
@@ -189,17 +188,17 @@ public class IhsIngestionJob extends Model {
 				.orderBy("ingestionJobID desc")
 				.setMaxRows(maxrecord)
 				.findList();
-		
+
 		return ihsIngestionJobs;
 	}
-	
+
 	public static void updateJobId(SingestionJobStatus singestionJobStatus, IhsIngestionJob ihsIngestionJob ){
-		
-		SqlUpdate update = Ebean.createSqlUpdate(upDateSql);  
+
+		SqlUpdate update = Ebean.createSqlUpdate(upDateSql);
 		update.setParameter("ingestionJobStatusID", singestionJobStatus.ingestionJobStatusID);
 		update.setParameter("ingestionJobID", ihsIngestionJob.ingestionjobID);
 		Ebean.execute(update);
-		
+
 	}
-	
+
 }
