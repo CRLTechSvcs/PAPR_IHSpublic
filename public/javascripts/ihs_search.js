a831718eacafe9e758b8c438941fb21c7efce47a
Andy Elliott <aelliott@crl.edu>
2016-11-29 16:24:30 -0600

    Enhancements #22-23: 'Control behavior should match that on Search Journals screen'

--- public/javascripts/ihs_search.js
+++ public/javascripts/ihs_search.js
@@ -51,6 +51,95 @@
 	}
 
 
+  // function drawPubRange() was moved 2016-11-28 by AJE here from advance_history.js, since advance_linking.js seems to need it too
+	function drawPubRange(currentTitle){
+	  console.warn('in ihs_search.js, drawPubRange(',currentTitle,')');
+		var pubRange ="";
+		var timerange = ' ' ;
+		for(var i = 0; i < currentTitle.publicationRangeViews.length; i++) {
+			for(var j = 0; j < periodicityType.length; j++){
+				if(periodicityType[j].id == currentTitle.publicationRangeViews[i].pubRangeId){
+					pubRange = periodicityType[j].name;
+				}
+			}
+      timerange +=
+      '<div style="float:left;">' +
+        currentTitle.publicationRangeViews[i].startDate +' to ' +
+        currentTitle.publicationRangeViews[i].endDate +
+      '</div><br/>' +
+      '<div style="padding-left:20px;"> ' + pubRange + '</div>' +
+      '<hr/>';
+    } // end for i
+    return timerange;
+	}
+
+
+
+  // function drawCurrentTitle() was moved 2016-11-23 by AJE here from advance_history.js, since advance_linking.js seems to need it too
+	function drawCurrentTitle(){
+    var strLen=25;
+		var tmptitle = "";
+		if(currentTitle.title.length > strLen + 1){
+			tmptitle = currentTitle.title.substring(0,strLen);
+		} else {
+			tmptitle = currentTitle.title;
+		}
+
+		var publisherName;
+		for(var i = 0; i< publisher.length; i++){
+			if (publisher[i].id == currentTitle.publisher ){
+				publisherName=publisher[i].name;
+			}
+		}
+
+		var innerhtml =
+		  '<div id="current" >'+
+      '<div class="history-title1">'+
+        '<span id="current-title" title="'+ currentTitle.title +'">'+ tmptitle + '</span>'+
+        '&emsp;&emsp;(Current Version)'+
+      '</div>';
+
+    innerhtml +=
+    '<div class="history-title2" id="edit-button">'+
+      '<a href="javascript:drawEditCurrentTitle();" id="edit-current" title="Edit the Current Version">'+
+      '<img src="/assets/images/edit.gif" /></a>'+
+    '</div>';
+
+    innerhtml +=
+    '<div class="history-col1" >'+
+      '<ul class="no-decoration">'+
+        '<li class="edit-only"><strong>Title:</strong> <span ></span></li>'+
+        '<li class="edit-only"><strong>Alpha Title:</strong> <span >Africa Today.</span></li>'+
+        '<li><strong>Publisher:</strong> <span >' + publisherName +'</span></li>'+
+        '<li><strong>Print ISSN:</strong> <span >'+currentTitle.printISSN +'</span></li>'+
+        '<li><strong>Electronic ISSN:</strong> <span >'+currentTitle.eISSN +'</span></li>'+
+        '<li><strong>OCLC Number:</strong> <span >'+currentTitle.oclcNumber +'</span></li>'+
+        '<li><strong>LCCN:</strong> <span>'+currentTitle.lccn +'</span></li>'+
+        '<li><strong>Note:</strong> <span >'+currentTitle.note +'</span></li>'+
+        '<li><strong>Image Page Ratio:</strong> <span >'+currentTitle.imagePageRatio +'</span></li>'+
+        '<li><strong>Publication Range:</strong> '+currentTitle.publicationRange +'</li>'+
+        '<li><strong>Publication Language:</strong> '+currentTitle.language +'</li>'+
+        '<li><strong>Publication Country:</strong> '+currentTitle.country +'</li>'+
+      '</ul>'+
+    '</div>';
+
+      innerhtml +=
+        '<div class="history-col2" style="padding:0px 10px;">'+
+          '<strong>Frequency Ranges:</strong><br /><br />'+
+          '<div class="freq_pub_range">'+ drawPubRange(currentTitle) +'</div>'+
+        '</div>'+
+				'</div>' + // closes id="current" ?
+				'<br/><br/><br/><br/><br/><br/>'+
+				'<div>' +
+					'<hr/>&emsp;<strong>Date Changed:'+ currentTitle.changeDate + '</strong>'+
+					'&emsp;&emsp;<strong>Changed by User:'+ currentTitle.changeUser + ' </strong>'+
+					'&emsp;&emsp;<strong>Changed by Member:'+ currentTitle.changeMember + ' </strong>'+
+					'<hr />'+
+				'</div>';
+
+    return innerhtml;
+
+	}  // end drawCurrentTitle()
 
 
 
@@ -123,82 +212,108 @@ function clearUnusedSearchFields(calling_function){
   // AJE 2016-09-21 populateSearchList used to live in search_home.js
 	function populateSearchList(response, ioArgs) {
 
-	  //console.warn('ihs_search.js, enter populateSearchList(response=', response, ' ; ioArgs=', ioArgs); // AJE 2016-09-16 testing
-    hideWaiting(); // AJE 2016-10-27
+    /* AJE 2016-11-23 shunt over to similar functions in other files ; the names are new but the content is more or less Travant's: may never have worked       advanced_editing_history */
+    var page = location.href;
+    var id = response.items[0].titleId;
+    console.info('ihs_search.js, populateSearchList(',response, ',', ioArgs,'), \n page=', page);
+    //console.info('(page.indexOf("advanced_editing_history") > -1) is ', (page.indexOf("advanced_editing_history") > -1));
+    //console.info('(page.indexOf("advanced_editing_history") > -1) is ', (page.indexOf("advanced_editing_linking") > -1))
 
-    var results = document.getElementById('results');
+    if (page.indexOf("advanced_editing_history") > -1) {
+      console.info('ihs_search.js, populateSearchList, exiting with populateJournalDetail_adv_hist(',response, ',', ioArgs,')');
+      populateSearchList_adv_hist(response, ioArgs); // advance_history.js
+      return;
+    } else if (page.indexOf("advanced_editing_linking") > -1) {
+      console.info('ihs_search.js, populateSearchList, exiting with populateJournalDetail_adv_link(',response, ',', ioArgs,')');
+      populateSearchList_adv_link(response, ioArgs); // advance_linking.js
+      return;
+    } /* AJE 2016-11-23 end shunt */
 
-    // AJE 2016-11-01 clear unused search boxes
-    //console.warn('ihs_search.js populateSearchList clears search boxes: ioArgs.url = "', ioArgs.url, '"; ioArgs.url.indexOf(browseJournalByTitle) == ', ioArgs.url.indexOf('browseJournalByTitle'));
-    var search_box = '';
-    if(ioArgs.url.indexOf('browseJournalByTitle') != -1 ){
-      search_box = document.getElementById('browse_titleid');
-    } else if(ioArgs.url.indexOf('containsJournalByTitle') != -1 ){
-      search_box = document.getElementById('contains_titleid');
-    } else if(ioArgs.url.indexOf('searchJournalByTitle') != -1 ){
-      search_box = document.getElementById('titleid');
-    } else if(ioArgs.url.indexOf('searchJournalByISSN') != -1 ){
-      search_box = document.getElementById('issnid');
-    } else if(ioArgs.url.indexOf('searchJournalByOCLC') != -1 ){
-      search_box = document.getElementById('oclcid');
-    }
-    var search_value = search_box.value;
-    // end AJE 2016-11-01
+  	  //console.warn('ihs_search.js, enter populateSearchList(response=', response, ' ; ioArgs=', ioArgs); // AJE 2016-09-16 testing
+      hideWaiting(); // AJE 2016-10-27
 
-    var ul = document.getElementById('search-list');
-    if (ul != null) { results.removeChild(ul); }
-    ul = document.createElement('ul');
-    ul.setAttribute('id', 'search-list');
+      var results = document.getElementById('results');
 
-    /* AJE 2016-10-26 handle searches where no results were found */
-    //console.log("response.items.length = " +response.items.length+ " items in populateSearchList ; r.i[0].titleID = " +response.items[0].titleID+ ".");
-    if ( (response.items.length < 1) ||
-         ((response.items[0].titleId == 0) || (response.items[0].titleId == undefined) || (response.items[0].titleId == ""))
-       ) {
-      //console.log("populateSearchList hit the IF");
-      var li = document.createElement('li');
-      li.innerHTML = "<h2>No results were found for <span class='search_term'>&nbsp;'"+search_value+"'&nbsp;</span></h2>";
-      ul.appendChild(li);
-    } else {
-      //console.log("populateSearchList hit the ELSE");
-      for (i = 0; i < response.items.length; i++) {
+      // AJE 2016-11-01 clear unused search boxes
+      //console.warn('ihs_search.js populateSearchList clears search boxes: ioArgs.url = "', ioArgs.url, '"; ioArgs.url.indexOf(browseJournalByTitle) == ', ioArgs.url.indexOf('browseJournalByTitle'));
+      var search_box = '';
+      if(ioArgs.url.indexOf('browseJournalByTitle') != -1 ){
+        search_box = document.getElementById('browse_titleid');
+      } else if(ioArgs.url.indexOf('containsJournalByTitle') != -1 ){
+        search_box = document.getElementById('contains_titleid');
+      } else if(ioArgs.url.indexOf('searchJournalByTitle') != -1 ){
+        search_box = document.getElementById('titleid');
+      } else if(ioArgs.url.indexOf('searchJournalByISSN') != -1 ){
+        search_box = document.getElementById('issnid');
+      } else if(ioArgs.url.indexOf('searchJournalByOCLC') != -1 ){
+        search_box = document.getElementById('oclcid');
+      }
+      var search_value = search_box.value;
+      // end AJE 2016-11-01
+
+      var ul = document.getElementById('search-list');
+      if (ul != null) { results.removeChild(ul); }
+      ul = document.createElement('ul');
+      ul.setAttribute('id', 'search-list');
+
+      /* AJE 2016-10-26 handle searches where no results were found */
+      //console.log("response.items.length = " +response.items.length+ " items in populateSearchList ; r.i[0].titleID = " +response.items[0].titleID+ ".");
+      if ( (response.items.length < 1) ||
+           ((response.items[0].titleId == 0) || (response.items[0].titleId == undefined) || (response.items[0].titleId == ""))
+         ) {
+        //console.log("populateSearchList hit the IF");
         var li = document.createElement('li');
-        var a = document.createElement('a');
-
-        console.log(i +') response.items[',i,'].title: "', response.items[i].title, ' *.publisher: "', response.items[i].publisher, ' ; response.items[i] = ', response.items[i]);// AJE 2016-09-20
-        var display_title = response.items[i].title + " / " + response.items[i].publisher;  // AJE 2016-10-25
-        //a.innerHTML = response.items[i].title; // Travant original
-        a.innerHTML = display_title; // AJE 2016-10-25
-        a.setAttribute('href', 'javascript:getJournalDetail(' + response.items[i].titleId + ');');
-        //a.setAttribute('title', response.items[i].title); // Travant original
-        a.setAttribute('title', display_title); // AJE 2016-10-25
-
-        a.setAttribute('id', response.items[i].titleId); // AJE 2016-11-04
-
-        li.appendChild(a);
+        li.innerHTML = "<h2>No results were found for <span class='search_term'>&nbsp;'"+search_value+"'&nbsp;</span></h2>";
         ul.appendChild(li);
-      } // end for
-    } // end else
+      } else {
+        //console.log("populateSearchList hit the ELSE");
+        for (i = 0; i < response.items.length; i++) {
+          var li = document.createElement('li');
+          var a = document.createElement('a');
 
-    results.appendChild(ul);
+          console.log(i +') response.items[',i,'].title: "', response.items[i].title, ' *.publisher: "', response.items[i].publisher, ' ; response.items[i] = ', response.items[i]);// AJE 2016-09-20
+          var display_title = response.items[i].title + " / " + response.items[i].publisher;  // AJE 2016-10-25
+          //a.innerHTML = response.items[i].title; // Travant original
+a.innerHTML = display_title + "ihs_search.js, populateSearchList a.setAttribute next"; // AJE 2016-10-25
 
-    //results.style.visibility = "visible"; // AJE 2016-09-16 change this line from Travant ... to
-    results.style.display = "block"; // ... this; now do some more toggling, call new AJE function, in ihs_search.js
-    toggle_search_home_title_components('populateSearchList');
-    // AJE : resume Travant original
+          /* AJE 2016-11-23 shunt over to similar functions in other files ; the names are new but the content is more or less Travant's: may never have worked       advanced_editing_history */
+          if (page.indexOf("advanced_editing_history") > -1) {
+            console.info('ihs_search.js, populateSearchList, will do javascript:getJournalDetail_adv_hist(' + response.items[i].titleId + ');');
+            a.setAttribute('href', 'javascript:getJournalDetail_adv_hist(' + response.items[i].titleId + ');');
+          } else if (page.indexOf("advanced_editing_linking") > -1) {
+            console.info('ihs_search.js, populateSearchList, will do javascript:getJournalDetail_adv_link(' + response.items[i].titleId + ');');
+            a.setAttribute('href', 'javascript:getJournalDetail_adv_link(' + response.items[i].titleId + ');');
+          } else { // default case
+            //a.setAttribute('title', response.items[i].title); // Travant original
+            a.setAttribute('href', 'javascript:getJournalDetail(' + response.items[i].titleId + ');');
+          } /* AJE 2016-11-23 end shunt */
 
-    // AJE 2016-11-04 : enhancements list #6: if only 1 title returned, display its detail screen immediately (no clicking on the title)
-    //console.warn("populateSearchList response.items.length == ", response.items.length);
-    if((response.items.length == 1)
-      && ((response.items[0].titleId != 0) && (response.items[0].titleId != ""))
-    ){
-      var thisTitleID = new String(response.items[0].titleId);
-      thisTitleID = thisTitleID.replace(" ", "");
-      //console.warn("enhancements list #6, response.items[",i,"].titleId = '", response.items[i].titleId, "' ; thisTitleID = '", thisTitleID, "' after js.replace.");
-      //console.warn("ihs_search.js, populateSearchList will call getJournalDetail("+thisTitleID+").");
-      getJournalDetail(thisTitleID); // search_home.js
-    } //end enhancements list #6: if(response.items.length == 1) AJE 2016-11-04
+          a.setAttribute('title', display_title); // AJE 2016-10-25
+          a.setAttribute('id', response.items[i].titleId); // AJE 2016-11-04
 
+          li.appendChild(a);
+          ul.appendChild(li);
+        } // end for
+      } // end else
+
+      results.appendChild(ul);
+
+      //results.style.visibility = "visible"; // AJE 2016-09-16 change this line from Travant ... to
+      results.style.display = "block"; // ... this; now do some more toggling, call new AJE function, in ihs_search.js
+      toggle_search_home_title_components('populateSearchList');
+      // AJE : resume Travant original
+
+      // AJE 2016-11-04 : enhancements list #6: if only 1 title returned, display its detail screen immediately (no clicking on the title)
+      //console.warn("populateSearchList response.items.length == ", response.items.length);
+      if((response.items.length == 1)
+        && ((response.items[0].titleId != 0) && (response.items[0].titleId != ""))
+      ){
+        var thisTitleID = new String(response.items[0].titleId);
+        thisTitleID = thisTitleID.replace(" ", "");
+        //console.warn("enhancements list #6, response.items[",i,"].titleId = '", response.items[i].titleId, "' ; thisTitleID = '", thisTitleID, "' after js.replace.");
+        //console.warn("ihs_search.js, populateSearchList will call getJournalDetail("+thisTitleID+").");
+        getJournalDetail(thisTitleID); // search_home.js
+      } //end enhancements list #6: if(response.items.length == 1) AJE 2016-11-04
 	}// end populateSearchList, moved by AJE from search_home.js 2016-09-21
 
 
