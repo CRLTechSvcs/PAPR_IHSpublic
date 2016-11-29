0814ad84c1c2afbd10c37168092f914939d4590f
Andy Elliott <aelliott@crl.edu>
2016-11-29 16:22:18 -0600

    whitespace and debgging comments for BuildTitleVersionView

--- app/controllers/AdvancedEditing.java
+++ app/controllers/AdvancedEditing.java
@@ -55,11 +55,17 @@ public class AdvancedEditing extends Controller {
 	}
 
 	public static List<TitleVersionView> BuildTitleVersionView (int titleid){
-		
+
+    /* AJE debugging 2016-11-28 */
+Logger.info("AdvancedEditing.java: BuildTitleVersionView(" +Integer.toString(titleid)+").");
+
+
 		IhsTitle ihsTitle = IhsTitle.find.fetch("ihsPublisher")
 				.fetch("ihsPublicationRange").fetch("ihsUser")
 				.fetch("ihsUser.ihsMember").where().eq("titleID", titleid)
 				.findUnique();
+
+Logger.info("BuildTitleVersionView: ihsTitle.toString(): " +ihsTitle.toString()+".");
 
 		List<TitleVersionView> titleVersionViews = new ArrayList<TitleVersionView>();
 
@@ -70,17 +76,32 @@ public class AdvancedEditing extends Controller {
 				.fetch("ihsUser.ihsMember").where().eq("titleID", titleid)
 				.orderBy("titleversionID desc").findList();
 
+Logger.info("BTVV: ihsTitleVersions.toString(): " +ihsTitleVersions.toString()+".");
+    //Logger.info("BTVV ... ihsTitleVersions.get(0).toString(): " +ihsTitleVersions.get(0).toString()+".");
+    Logger.info("BTVV ... ihsTitleVersions has " +ihsTitleVersions.size()+" elements.");
+
 		for (IhsPublicationRange ihsPublicationRange : ihsTitle.ihsPublicationRange) {
+
+      Logger.info("BTVV: for (1) IhsPublicationRange : ihsTitle.ihsPublicationRange.");
+      Logger.info("BTVV ... ihsTitle.ihsPublicationRange.toString(): " +ihsTitle.ihsPublicationRange.toString()+".");
+      Logger.info("BTVV ... ihsTitle.ihsPublicationRange.get(0).toString(): " +ihsTitle.ihsPublicationRange.get(0).toString()+".");
+      Logger.info("BTVV ... ihsTitle.ihsPublicationRange has " +ihsTitle.ihsPublicationRange.size()+" elements.");
 
 			String endDate = ihsPublicationRange.endDate == null ? "" : dtf
 					.print(ihsPublicationRange.endDate);
+
+      Logger.info("BTVV for (1) endDate=" +endDate+" ; ihsPublicationRange.publicationRangeID=" +Integer.toString(ihsPublicationRange.publicationRangeID)+".");
+
 
 			publicationRangeViews.add(new PublicationRangeView(
 					ihsPublicationRange.publicationRangeID, dtf
 							.print(ihsPublicationRange.startDate), endDate,
 					ihsPublicationRange.speriodicityType.periodicityTypeID));
 
-		}
+		} // end for IhsPublicationRange
+
+      Logger.info("BTVV: for (1) publicationRangeViews.add completed.");
+
 
 		String publicationRange = Helper
 				.getPublicationRange(ihsTitle.ihsPublicationRange);
@@ -89,22 +110,32 @@ public class AdvancedEditing extends Controller {
 				.substring(0, 25) : ihsTitle.ihsPublisher.name;
 
 		String imagePageRatio = ihsTitle.imagePageRatio != null ? ihsTitle.imagePageRatio.toString(): "";
-		
+
 		titleVersionViews.add(new TitleVersionView(ihsTitle.titleVersionID,
 				ihsTitle.titleID, ihsTitle.title, ihsTitle.alphaTitle,
 				ihsTitle.ihsPublisher.publisherID, tmpPublisherName,
 				ihsTitle.printISSN, ihsTitle.eISSN, ihsTitle.oclcNumber,
-				ihsTitle.lccn == null ?"" : ihsTitle.lccn, 
+				ihsTitle.lccn == null ?"" : ihsTitle.lccn,
 				ihsTitle.description, publicationRange,
 				publicationRangeViews, "Y", dtf.print(ihsTitle.changeDate),
 				ihsTitle.ihsUser.userName, ihsTitle.ihsUser.ihsMember.name, imagePageRatio,
 				ihsTitle.language,ihsTitle.country));
 
+    Logger.info("BTVV: for (1) titleVersionViews.add completed; titleVersionViews.toString(): " +titleVersionViews.toString()+".");
+    Logger.info("BTVV ... titleVersionViews[0].get(0).toString(): " +titleVersionViews.get(0).toString()+".");
+    Logger.info("BTVV ... titleVersionViews has " +titleVersionViews.size()+" elements.");
+
+    Logger.info("BTVV: for 2) is next; titleVersionViews.add completed; titleVersionViews.toString(): " +titleVersionViews.toString()+".");
+
 		for (IhsTitleVersion ihsTitleVersion : ihsTitleVersions) {
+
+      Logger.info("BTVV: for 2) IhsTitleVersion : ihsTitleVersions begins.");
 
 			List<PublicationRangeView> publicationRangeViewsVer = new ArrayList<PublicationRangeView>();
 
 			for (IhsPublicationRangeVer ihsPublicationRangeVer : ihsTitleVersion.ihsPublicationRangeVer) {
+
+      Logger.info("BTVV: for 3) IhsPublicationRangeVer : ihsTitleVersion.ihsPublicationRangeVer.");
 
 				String endDate = ihsPublicationRangeVer.endDate == null ? ""
 						: dtf.print(ihsPublicationRangeVer.endDate);
@@ -119,8 +150,12 @@ public class AdvancedEditing extends Controller {
 			}
 
 			String imagePageRatioVer = ihsTitleVersion.imagePageRatio != null ? ihsTitleVersion.imagePageRatio.toString(): "";
-			
+
 			if (ihsTitle.titleVersionID != ihsTitleVersion.titleversionID) {
+
+        Logger.info("BTVV, IF (ihsTitle.titleVersionID ["+Integer.toString(ihsTitle.titleVersionID)+"] != ihsTitleVersion.titleversionID ["+Integer.toString(ihsTitleVersion.titleversionID)+"])");
+        Logger.info("... will .add(new TitleVersionView...");
+
 				titleVersionViews
 						.add(new TitleVersionView(
 								ihsTitleVersion.titleversionID,
@@ -138,18 +173,24 @@ public class AdvancedEditing extends Controller {
 								publicationRangeViewsVer, "N", dtf
 										.print(ihsTitleVersion.changeDate),
 								ihsTitleVersion.ihsUser.userName,
-								ihsTitleVersion.ihsUser.ihsMember.name, 
+								ihsTitleVersion.ihsUser.ihsMember.name,
 								imagePageRatioVer,
 								ihsTitleVersion.language,ihsTitleVersion.country));
+			} else { // AJE 2016-11-28 tis else block is just debugging
+        Logger.info("BTVV: ELSE block of if (ihsTitle.titleVersionID ["+Integer.toString(ihsTitle.titleVersionID)+"] != ihsTitleVersion.titleversionID ["+Integer.toString(ihsTitleVersion.titleversionID)+"])");
+        Logger.info("... no action will take place; this block exists only to remind you of that");
 			}
 
-		}
+		} // end for IhsTitleVersion
+
+		Logger.info("BTVV at end, titleVersionViews="+titleVersionViews);
+    Logger.info("BTVV ... titleVersionViews has " +titleVersionViews.size()+" elements.");
+
 		return titleVersionViews;
 	}
-	
+
 	public static Result GetTitles(int titleid) {
-
-
+    Logger.info("Advanced Editing.java, GetTitles(int titleid) will just call BuildTitleVersionView(titleid)");
 		return ok(toJson(BuildTitleVersionView(titleid)));
 	}
 
@@ -173,7 +214,7 @@ public class AdvancedEditing extends Controller {
 		return ok(toJson(periodicityTypeViews));
 	}
 
-	
+
 	public static void postTitleTran(TitleVersionView titleVersionView) {
 
 		String user = session().get(Login.User);
@@ -187,7 +228,7 @@ public class AdvancedEditing extends Controller {
 
 		/*------------*/
 
-		
+
 		if (ihsTitle.titleVersionID == 0) {
 			IhsTitleVersion ihsTitleVersion = new IhsTitleVersion(
 					ihsTitle.titleID, ihsTitle.title, ihsTitle.alphaTitle,
@@ -219,14 +260,14 @@ public class AdvancedEditing extends Controller {
 
 		ihsTitle.ihsPublisher = ihsPublisher;
 		ihsTitle.imagePageRatio = titleVersionView.imagePageRatio.equals("") ? null : new Integer( titleVersionView.imagePageRatio );
-		
+
 		ihsTitle.titleVersionID = 0;
 
 		ihsTitle.setIhsUser(ihsUser);
 		ihsTitle.changeDate = new DateTime();
 		ihsTitle.language = titleVersionView.language;
 		ihsTitle.country = titleVersionView.country;
-		
+
 		ihsTitle.update();
 
 		/*------------*/
@@ -295,7 +336,7 @@ public class AdvancedEditing extends Controller {
 		return ok();
 	}
 
-	
+
 	public static Result restorePreviousTitle(int titleId, int titleVersionId) {
 
 		String user = session().get(Login.User);
@@ -312,7 +353,7 @@ public class AdvancedEditing extends Controller {
 
 		/*------------*/
 
-		
+
 		if (ihsTitle.titleVersionID == 0) {
 			IhsTitleVersion ihsTitleVersionNew = new IhsTitleVersion(
 					ihsTitle.titleID, ihsTitle.title, ihsTitle.alphaTitle,
@@ -329,10 +370,10 @@ public class AdvancedEditing extends Controller {
 						ihsPublicationRange.endDate)).save();
 			}
 		}
-		
-		
+
+
 		ihsTitle.setTitle(ihsTitleVersion.title);
-		
+
 		ihsTitle.alphaTitle = ihsTitleVersion.alphaTitle;
 		ihsTitle.printISSN = ihsTitleVersion.printISSN;
 		ihsTitle.eISSN = ihsTitleVersion.eISSN;
@@ -347,17 +388,17 @@ public class AdvancedEditing extends Controller {
 		ihsTitle.titleVersionID = ihsTitleVersion.titleversionID;
 
 		ihsTitle.changeDate = ihsTitleVersion.changeDate;
-		
+
 		ihsTitle.imagePageRatio = ihsTitleVersion.imagePageRatio;
 
 		ihsTitle.language = ihsTitleVersion.language;
-		
+
 		ihsTitle.country = ihsTitleVersion.country;
-		
-		
+
+
 		ihsTitle.update();
 
-		
+
 		/*
 		for (IhsPublicationRange ihsPublicationRange : ihsTitle.ihsPublicationRange) {
 			ihsPublicationRange.delete();
@@ -449,7 +490,12 @@ public class AdvancedEditing extends Controller {
 		List<IhsTitle> ihsTitleParents = IhsTitle.find
 				.fetch("ihsPublicationRange").where().in("titleID", parentIds)
 				.findList();
+
+		Logger.info("AdvancedEditing.java, getLinkView("+Integer.toString(titleid)+") ; next is for ihsTitleParents.");
+
 		for (IhsTitle ihsTitlePar : ihsTitleParents) {
+
+		Logger.info("...ihsTitlePar.title = "+ihsTitlePar.title);
 
 			linkView.parents
 					.add(new LinkViewParentChild(
