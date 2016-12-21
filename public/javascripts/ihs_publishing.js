cd4ad43ca52f87654ecfb7f482fdc457ab4be1ae
Andy Elliott <aelliott@crl.edu>
2016-12-20 18:47:48 -0600

    fixes related to date formats on publishing_publish_new_data.scala.html with use of new Dojo dijit/form/DateTextBox

--- public/javascripts/ihs_publishing.js
+++ public/javascripts/ihs_publishing.js
@@ -28,22 +28,50 @@ function submitPub(){
 	var fileFormat = $('#fileFormat').val();
 	var startDate =  $('#startDate').val();
 	var endDate =  $('#endDate').val();
-	//console.info('submitPub begins with startDate=' +startDate+ '\nendDate='+endDate);
+	console.log('ihs_publishing : submitPub starts with startDate=' +startDate+ ' ; endDate='+endDate);
+
+// AJE 2016-11-21 reformat the date value so it goes nicely into new DateTime(publishingView.startDate) in Publishing.java
+/*
+  var startYear = startDate.substr(6);
+  var startMonth = startDate.substr(0, 2);
+  var startDay = startDate.substr(3, 2);
+*/
+  var startYear = startDate.substring( startDate.lastIndexOf("/")+1);
+  var startMonth = startDate.substring(0, startDate.indexOf("/"));
+  console.log('for month, startDate.substring(0)=', startDate.substring(0), ' ; startDate.indexOf("/")-1=', startDate.indexOf("/")-1, ' ; and the character there is ', startDate.charAt(startDate.indexOf("/")-1), '.');
+  console.log('for month, expect ', startDate.substring(0, startDate.indexOf("/")), '.');
+  console.log('startDate.indexOf("/")+1 = ', startDate.indexOf("/")+1);
+  var startDay = startDate.substring(startDate.indexOf("/")+1, startDate.lastIndexOf("/"));
+  console.log('for day, expect from ', startDate.indexOf("/")+1, ' to ', startDate.lastIndexOf("/")-1, '.');
+
+  startDate =  startYear +"-"+  startMonth +"-"+ startDay;
+	console.log('1) submitPub has startYear=', startYear, ' ; startMonth=', startMonth, " ; startDay = ", startDay, " ; startDate = ", startDate);
+
+  var endYear = endDate.substring( endDate.lastIndexOf("/")+1);
+  var endMonth = endDate.substring(0, endDate.indexOf("/"));
+  var endDay = endDate.substring(endDate.indexOf("/")+1, endDate.lastIndexOf("/"));
+  endDate =  endYear +"-"+  endMonth +"-"+ endDay;
+	console.log('2) submitPub has endYear=', endYear, ' ; endMonth=', endMonth, " ; endDay = ", endDay, "; endDate = ", endDate);
 
 	var errorMessage = '';
 
 	if(jobName == ''){
-		errorMessage += 'Enter a  valid Job Name \n' ;
+	  console.log('ihs_publishing.js : submitPub has no valid jobName');
+		errorMessage += 'Enter a valid Job Name \n' ;
 	}
 
 	if(startDate != ''){
-		if( !validateDate(startDate) ){ // function validateDate: see ihs_common.js
-			errorMessage += 'Enter a  valid Start Date \n' ;
+	  var dateToValidate = startMonth + "/" + startDay + "/" +startYear; // AJE 2016-12-20 : prepre a version format that function validateDate will like
+		//if( !validateDate(startDate) ){ // Travant original
+		if( !validateDate(dateToValidate) ){ // function validateDate: see ihs_common.js
+			errorMessage += 'Enter a valid Start Date \n' ;
 		}
 	}
 
 	if( endDate != ''){
-		if( !validateDate(endDate) ){
+	  var dateToValidate = endMonth + "/" + endDay + "/" +endYear; // AJE 2016-12-20 : prepre a version format that function validateDate will like
+		// if( !validateDate(endDate) ){ // Travant original
+		if( !validateDate(dateToValidate) ){ // function validateDate: see ihs_common.js
 			errorMessage += 'Enter a  valid End Date \n' ;
 		}
 	}
@@ -53,29 +81,12 @@ function submitPub(){
 		return;
 	}
 
-// AJE 2016-11-21 reformat the date value so it goes nicely into new DateTime(publishingView.startDate) in Publishing.java
-  var startYear = startDate.substr(6);
-  var startMonth = startDate.substr(0, 2);
-  var startDay = startDate.substr(3, 2);
-  startDate =  startYear +"-"+  startMonth +"-"+ startDay;
-	//alert('1) submitPub continues with startYear=' +startYear+ '\nstartMonth='+startMonth+ "\nstartDay = " +startDay+ "\n\nstartDate = " +startDate);
-
-  var endYear = endDate.substr(6);
-  var endMonth = endDate.substr(0, 2);
-  var endDay = endDate.substr(3, 2);
-  endDate =  endYear +"-"+  endMonth +"-"+ endDay;
-	//alert('2) submitPub continues with endYear=' +endYear+ '\nendMonth='+endMonth+ "\nendDay = " +endDay+ "\n\nendDate = " +endDate);
-	console.info('3) submitPub continues with startDate=' +startDate+ '\nendDate='+endDate);
-	//alert('3) submitPub continues with startDate=' +startDate+ '\nendDate='+endDate);
-
-// AJE 2016-11-21
-
 
 	publishingView.jobName = jobName;
 	publishingView.fileFormat = fileFormat;
 	publishingView.startDate = startDate;
 	publishingView.endDate = endDate;
-  //alert('startDate = ' +startDate+ '\n endDate = ' +endDate+ '\npublishingView.startDate = ' +publishingView.startDate+ '\npublishingView.endDate = '+publishingView.endDate);
+  console.log('startDate = ', startDate, ' ; endDate = ', endDate, ' ; publishingView.startDate = ', publishingView.startDate, ' ; publishingView.endDate = ', publishingView.endDate);
 
 	showWaiting();
 
@@ -90,13 +101,11 @@ function submitPub(){
 
         load: function(data) {
         	hideWaiting();
-   	 		window.location="/publishing_published_data";
-
+   	 		  window.location="/publishing_published_data";
         },
         error: function(error) {
         	 hideWaiting();
-            alert("ihs_publishing.js, postPublishingView Error:" + error);
+           alert("ihs_publishing.js, postPublishingView Error:" + error);
         }
     });
-
 }
